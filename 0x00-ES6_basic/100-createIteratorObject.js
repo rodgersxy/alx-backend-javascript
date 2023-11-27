0c8da0530e432e085c9568ab768f1749bfcbb4ff
rodgersxy <rodgersny99@gmail.com>
2023-11-27 17:09:47 +0000

    100-createIteratorObject.js

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+/* iterator object for iterating employees */
+export default function createIteratorObject(report) {
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
+      }
+    }
+  }());
+}
