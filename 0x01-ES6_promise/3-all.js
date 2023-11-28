5063cb0104d42c9d3c5c4dcb6b18b85b84e84b83
rodgersxy <rodgersny99@gmail.com>
2023-11-28 16:28:34 +0000

    3-all.js

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+/* imports two functions, uploadPhoto and createUser */
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
