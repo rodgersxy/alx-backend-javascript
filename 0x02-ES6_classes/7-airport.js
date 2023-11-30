569864088c60918114164aa23176ddcc8ab5f8ef
rodgersxy <rodgersny99@gmail.com>
2023-11-30 15:14:25 +0000

    7-airport.js

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,10 @@
+/* class named Airport */
+export default class Airport {
+  constructor (name, code) {
+    this._name = name;
+    this._code = code;
+  }
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
